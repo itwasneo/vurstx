639a1c0da66738d423db5135012ed57a7d5ab4a7
itwasneo <kaan.yildiz@protonmail.com>
2023-03-05 00:20:55 +0300

    eb_client has the reading loop
    start_listening and stop_listening functions are added. There is an
    obvious problem in the stop_listening function. Due to the fact that the
    program stuck inside the reading loop, it can't be stopped any outside
    manipulation coming from the main thread.

--- src/components/eb_client.rs
+++ src/components/eb_client.rs
@@ -3,20 +3,20 @@ use bytes::{BufMut, BytesMut};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::collections::HashMap;
-use std::io::{Result, Write};
+use std::io::{Read, Result, Write};
 use std::net::{SocketAddr, TcpStream};
 
-struct EventBusClient<T> {
+type Handler = Box<dyn FnOnce() + Send + 'static>;
+
+pub struct EventBusClient {
     read_stream: TcpStream,
     write_stream: TcpStream,
-    handlers: HashMap<String, T>,
+    handlers: HashMap<String, Handler>,
     thread_pool: ThreadPool,
+    state: ClientState,
 }
 
-impl<T> EventBusClient<T>
-where
-    T: FnOnce() + Send + 'static,
-{
+impl EventBusClient {
     /// Returns a new __EventBusClient__ connected to the given Socket address.
     ///
     /// **worker_count** determines the thread count of the thread pool which will
@@ -29,6 +29,7 @@ where
             write_stream,
             handlers: HashMap::new(),
             thread_pool: ThreadPool::new(worker_count),
+            state: ClientState::Connected,
         })
     }
 
@@ -36,8 +37,9 @@ where
     /// also expects the **handler** function that will handle the Event Bus messages
     /// received specifically for the given **address**.
     ///
-    /// # Mental Note: You can think **address** as a **topic** name in a regular message queue.
-    pub fn register(&mut self, address: String, handler: T) -> Result<&Self> {
+    /// # Mental Note:
+    /// You can think **address** as a **topic** name in a regular message queue.
+    pub fn register(&mut self, address: String, handler: Handler) -> Result<&mut Self> {
         match self.write_packet(OutPacket::Register(Message {
             address: address.clone(),
             body: None,
@@ -53,7 +55,8 @@ where
 
     /// Unregisters the __EventBusClient__ from the given Event Bus **address**.
     ///
-    /// # Mental Note: You can think **address** as a **topic** name in a regular message queue.
+    /// # Mental Note:
+    /// You can think **address** as a **topic** name in a regular message queue.
     pub fn unregister(&mut self, address: String) -> Result<&Self> {
         match self.write_packet(OutPacket::Unregister(Message {
             address: address.clone(),
@@ -66,6 +69,44 @@ where
             }
             Err(e) => Err(e),
         }
+    }
+
+    /// Starts __EventBusClient__ to listen incoming Event Bus messages belonging to the **address*es
+    /// that are previously registered by the client.
+    ///
+    /// As long as the **state** value of the __EventBusClient__ is **Listening**, the client keeps
+    /// listening to the Event Bus messages.
+    pub fn start_listening(&mut self) {
+        if self.state == ClientState::Connected {
+            self.state = ClientState::Listening;
+            while self.state == ClientState::Listening {
+                let mut content_length_buffer = [0u8; 4];
+                match self.read_stream.read_exact(&mut content_length_buffer) {
+                    Ok(_) => {
+                        let content_length =
+                            u32::from_be_bytes(content_length_buffer.try_into().unwrap());
+                        let mut packet_buffer = BytesMut::zeroed(content_length as usize);
+                        match self.read_stream.read_exact(&mut packet_buffer) {
+                            Ok(_) => {
+                                self.thread_pool.execute(move || {
+                                    let msg: InPacket =
+                                        serde_json::from_slice(&packet_buffer).unwrap();
+                                    println!("{msg:?}");
+                                });
+                            }
+                            Err(_) => break,
+                        }
+                    }
+                    Err(_) => break,
+                }
+            }
+        }
+    }
+
+    /// Stops __EventBusClient__ to listen incoming Event Bus messages entirely by changing the
+    /// client's **state** value.
+    pub fn stop_listening(&mut self) {
+        self.state = ClientState::Stopped;
     }
 
     /// Writes the given TCP packet to the **write_stream** of the __EventBusClient__.
@@ -84,7 +125,6 @@ where
         self.write_stream.flush()
     }
 }
-
 /// Enum representing the out-going TCP packets. Any TCP packet belonging to this Enum
 /// meant to be sent **from** the __Event Bus client__ **to** the __remote Event Bus__.
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
@@ -116,4 +156,52 @@ struct Message {
     address: String,
     body: Option<Value>,
     headers: Option<HashMap<String, String>>,
+}
+
+#[derive(PartialEq)]
+enum ClientState {
+    Connected,
+    Listening,
+    Stopped,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{InPacket, Message, OutPacket};
+    use bytes::{BufMut, BytesMut};
+
+    const JSON_PING: &str = r#"{"type":"ping"}"#;
+    const JSON_PONG: &str = r#"{"type":"pong"}"#;
+    const JSON_REGISTER: &str = r#"{"type":"register", "address":"address"}"#;
+    const JSON_MESSAGE: &str = r#"{"type":"message", "address":"address"}"#;
+
+    #[test]
+    fn message_serde_tests() {
+        assert_eq!(OutPacket::Ping, serde_json::from_str(JSON_PING).unwrap());
+        assert_eq!(InPacket::Pong, serde_json::from_str(JSON_PONG).unwrap());
+        assert_eq!(
+            OutPacket::Register(Message {
+                address: "address".to_owned(),
+                body: None,
+                headers: None,
+            }),
+            serde_json::from_str(JSON_REGISTER).unwrap()
+        );
+        assert_eq!(
+            OutPacket::Send(Message {
+                address: "address".to_owned(),
+                body: None,
+                headers: None
+            }),
+            serde_json::from_str(JSON_MESSAGE).unwrap()
+        );
+    }
+
+    #[test]
+    fn adding_content_length_test() {
+        let mut buf = BytesMut::with_capacity(4 + JSON_PONG.len());
+        buf.put_u32(JSON_PONG.len() as u32);
+        buf.put(JSON_PONG.as_bytes());
+        assert_eq!(b"\0\0\0\x0f{\"type\":\"pong\"}"[..], buf);
+    }
 }
